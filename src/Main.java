2ab193df537a497a577f52c235f15fe017dc9308
Mateusz <zubermateusz@gmail.com>
2022-11-07 19:01:23 +0100

    dodano klasę Flight i metodę  getDetails

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,17 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+class Fight {
+    String departure;
+    String arrival;
+
+    public Fight(String departure, String arrival) {
+        this.departure = departure;
+        this.arrival = arrival;
+    }
+
+    public String getDetails() {
+        return "Flight from " + this.departure + " to " + this.arrival;
+    }
+}
